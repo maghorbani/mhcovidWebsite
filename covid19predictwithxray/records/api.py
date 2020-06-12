df3e3ce8eba0e800eab22034875314502c7ac4e6
Mohammad Ali Ghorbani <maghorbani@aut.ac.ir>
2020-06-12 11:49:09 +0430

    records/api.py is now actualy predicting the change of ژ COVID19 infection rather than using a random percent

--- covid19predictwithxray/records/api.py
+++ covid19predictwithxray/records/api.py
@@ -1,7 +1,11 @@
 from .models import Image
 from rest_framework import viewsets, permissions
 from .serializers import ImageSerializer
-import random
+# from MHCovid import MHCovid
+from .MHCovid import MHCovid
+import os
+import matplotlib
+
 
 # Image Viewset
 
@@ -20,8 +24,28 @@ class ImageViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         # print(serializer.validated_data['score'])
+
+        model_obj = None
+        if (self.request.user.is_authenticated):
+            model_obj = serializer.save(owner=self.request.user)
+        else:
+            model_obj = serializer.save()
+
+        matplotlib.use('agg')
+        matplotlib.pyplot.switch_backend('Agg')
+
+        fileName = model_obj.file.name
+        mh = MHCovid()
+        model = mh.generateModel()
+        model.load_weights(os.path.join(os.path.dirname(
+            os.path.abspath(__file__)), 'cmodel.h5'))
+        dirp = os.path.abspath(os.path.join(
+            os.path.dirname(__file__), '..'))
+        fileName = os.path.join(dirp, 'media', fileName)
+        pr = mh.predict(model, fileName)
+
         if (self.request.user.is_authenticated):
             serializer.save(owner=self.request.user,
-                            score=random.random() * 100)
+                            score=pr*100)
         else:
-            serializer.save(score=random.random() * 100)
+            serializer.save(score=pr*100)
